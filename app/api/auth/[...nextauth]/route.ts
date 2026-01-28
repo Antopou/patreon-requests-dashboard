93e5a173b627b090090f8df455efb64b23864c61
Gnan Antopou <gnanantopou@gmail.com>
2026-01-28 14:12:45 +0700

    Fix error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,19 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { JWT } from 'next-auth/jwt'
+import { Session } from 'next-auth'
+
+declare module 'next-auth' {
+  interface Session {
+    accessToken?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken?: string;
+  }
+}
 
 const handler = NextAuth({
   debug: true, // Enable debug mode
